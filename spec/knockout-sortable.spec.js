d01c71521f838020b4b3f648494d6a0a744ebc21
Ryan Niemeyer <rniemeyer@gmail.com>
2012-02-04 00:06:07 -0600

    first commit

---
+++ spec/knockout-sortable.spec.js
@@ -0,0 +1,321 @@
+describe("knockout-sortable", function(){
+    //helper engine that can use a template from a string
+    ko.stringTemplateEngine = function() {
+        var templates = { data: {} };
+
+        var stringTemplateSource = function(template) {
+            this.text = function(value) {
+                if (arguments.length === 0) {
+                    return templates[template];
+                }
+                templates[template] = value;
+            };
+        };
+
+        var templateEngine = new ko.nativeTemplateEngine();
+        templateEngine.makeTemplateSource = function(template) {
+            return new stringTemplateSource(template);
+        };
+
+        templateEngine.addTemplate = function(key, value) {
+            templates[key] = value;
+        };
+
+        return templateEngine;
+    };
+
+    var defaults = {
+        connectClass: ko.bindingHandlers.sortable.connectClass,
+        allowDrop: ko.bindingHandlers.sortable.allowDrop,
+        beforeMove: ko.bindingHandlers.sortable.beforeMove,
+        afterMove: ko.bindingHandlers.sortable.afterMove
+    };
+
+    var setup = function(options) {
+        ko.setTemplateEngine(options.engine || new ko.nativeTemplateEngine());
+        options.root = options.elems.first();
+        $("body").append(options.root);
+        options.root.hide();
+        ko.applyBindings(options.vm, options.root[0]);
+    };
+
+    describe("sortable binding", function() {
+        beforeEach(function() {
+            //restore defaults
+            ko.utils.extend(ko.bindingHandlers.sortable, defaults);
+        });
+
+        describe("when using an anonymous template", function(){
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: items'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]) }
+                };
+
+                setup(options);
+            });
+
+            it("should render all items", function(){
+                expect(options.root.children().length).toEqual(3);
+            });
+        });
+
+        describe("when using a named template", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: { template: \"itemTmpl\", data: items }'></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]) },
+                    engine: ko.stringTemplateEngine()
+                };
+
+                options.engine.addTemplate("itemTmpl", "<li></li>");
+                setup(options);
+            });
+
+            it("should render all items", function(){
+                expect(options.root.children().length).toEqual(3);
+            });
+        });
+
+        describe("when using the default options", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: items'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]) }
+                };
+
+                setup(options);
+            });
+
+            it("should add the default connectWith class 'ko_container' to the root element", function(){
+                expect(options.root.hasClass(defaults.connectClass)).toBeTruthy();
+            });
+
+            it("should call .sortable on the root element", function() {
+                expect(options.root.data("sortable")).toBeDefined();
+            });
+
+            it("should attach meta-data to the root element indicating the parent observableArray", function() {
+                expect(options.root.data("ko_sortList")).toEqual(options.vm.items);
+            });
+
+            it("should attach meta-data to child elements indicating their item", function() {
+                expect(options.root.children().first().data("ko_sortItem")).toEqual(options.vm.items()[0]);
+            });
+
+            it("should attach meta-data to child elements indicating their parent observableArray", function() {
+                expect(options.root.children().first().data("ko_parentList")).toEqual(options.vm.items);
+            });
+        });
+
+        describe("when setting allowDrop globally to false", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: items'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]) }
+                };
+
+                ko.bindingHandlers.sortable.allowDrop = false;
+                setup(options);
+            });
+
+            it("should not add the default connectWith class 'ko_container' to the root element", function(){
+                expect(options.root.hasClass(defaults.connectClass)).toBeFalsy();
+            });
+        });
+
+        describe("when setting allowDrop globally to an observable that is false", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: items'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]) }
+                };
+
+                ko.bindingHandlers.sortable.allowDrop = ko.observable(false);
+                setup(options);
+            });
+
+            it("should not add the default connectWith class 'ko_container' to the root element", function(){
+                expect(options.root.hasClass(defaults.connectClass)).toBeFalsy();
+            });
+
+            it("should add the default connectWith class after setting the observable to true", function() {
+                ko.bindingHandlers.sortable.allowDrop(true);
+                expect(options.root.hasClass(defaults.connectClass)).toBeTruthy();
+            });
+        });
+
+        describe("when setting allowDrop globally to a function", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: items'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]), disabled: function(list) { return list().length > 3; } }
+                };
+
+                ko.bindingHandlers.sortable.allowDrop = options.vm.disabled;
+                setup(options);
+            });
+
+            it("should not add the default connectWith class 'ko_container' to the root element", function(){
+                expect(options.root.hasClass(defaults.connectClass)).toBeFalsy();
+            });
+
+            it("should add the default connectWith class after setting the observable to true", function() {
+                options.vm.items.push(4);
+                expect(options.root.hasClass(defaults.connectClass)).toBeTruthy();
+            });
+        });
+
+        describe("when passing false for allowDrop in the binding options", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: { data: items, allowDrop: false }'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]) }
+                };
+
+                setup(options);
+            });
+
+            it("should not add the default connectWith class 'ko_container' to the root element", function(){
+                expect(options.root.hasClass(defaults.connectClass)).toBeFalsy();
+            });
+        });
+
+        describe("when passing an observable that is false for allowDrop in the binding options", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: { data: items, allowDrop: enabled }'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]), enabled: ko.observable(false) }
+                };
+
+                setup(options);
+            });
+
+            it("should not add the default connectWith class 'ko_container' to the root element", function(){
+                expect(options.root.hasClass(defaults.connectClass)).toBeFalsy();
+            });
+
+            it("should add the default connectWith class after setting the observable to true", function() {
+                options.vm.enabled(true);
+                expect(options.root.hasClass(defaults.connectClass)).toBeTruthy();
+            });
+        });
+
+        describe("when passing a function for allowDrop in the binding options", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: { data: items, allowDrop: disabled }'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]), disabled: function(list) { return list().length > 3; } }
+                };
+
+                setup(options);
+            });
+
+            it("should not add the default connectWith class 'ko_container' to the root element", function(){
+                expect(options.root.hasClass(defaults.connectClass)).toBeFalsy();
+            });
+
+            it("should re-evaluate the allowDrop function when any observables change and add the connectWith class, if appropriate", function() {
+                options.vm.items.push(4);
+                expect(options.root.hasClass(defaults.connectClass)).toBeTruthy();
+            });
+        });
+
+        describe("when overriding the connectWith class globally", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: items'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]) }
+                };
+
+                ko.bindingHandlers.sortable.connectClass = "mycontainer";
+                setup(options);
+            });
+
+            it("should not add the default connectWith class 'ko_container' to the root element", function(){
+                expect(options.root.hasClass(defaults.connectClass)).toBeFalsy();
+            });
+
+            it("should add the overriden connectWith class 'mycontainer' to the root element", function(){
+                expect(options.root.hasClass('mycontainer')).toBeTruthy();
+            });
+        });
+
+        describe("when overriding connectClass in the binding options", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: { data: items, connectClass: \"mycontainer\" }'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]) }
+                };
+
+                setup(options);
+            });
+
+            it("should not add the default connectWith class 'ko_container' to the root element", function(){
+                expect(options.root.hasClass(defaults.connectClass)).toBeFalsy();
+            });
+
+            it("should add the overriden connectWith class 'mycontainer' to the root element", function(){
+                expect(options.root.hasClass('mycontainer')).toBeTruthy();
+            });
+        });
+
+        describe("when passing extra options for .sortable in the binding", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: { data: items, options: { disabled: true } }'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]) }
+                };
+
+                setup(options);
+            });
+
+            it("should pass the option on to .sortable properly", function() {
+                expect(options.root.sortable("option", "disabled")).toBeTruthy();
+            });
+        });
+
+        describe("when setting extra options for .sortable globally", function() {
+            var options;
+
+            beforeEach(function() {
+                options = {
+                    elems: $("<ul data-bind='sortable: items'><li data-bind='text: $data'></li></ul>"),
+                    vm: { items: ko.observableArray([1, 2, 3]) }
+                };
+
+                ko.bindingHandlers.sortable.options = { disabled: true };
+
+                setup(options);
+            });
+
+            it("should pass the option on to .sortable properly", function() {
+                expect(options.root.sortable("option", "disabled")).toBeTruthy();
+            });
+        });
+    });
+});
---
+++ src/knockout-sortable.js
@@ -0,0 +1,137 @@
+//knockout-sortable | (c) 2012 Ryan Niemeyer | http://www.opensource.org/licenses/mit-license
+(function(ko, $, undefined) {
+var prepareTemplateOptions = function(valueAccessor) {
+    var result = {},
+        options = ko.utils.unwrapObservable(valueAccessor());
+
+    //build our options to pass to the template engine
+    if (options.data) {
+        result.foreach = options.data;
+        result.name = options.template;
+        result.afterAdd = options.afterAdd;
+        result.beforeRemove = options.beforeRemove;
+        result.afterRender = options.afterRender;
+        result.includeDestroyed = options.includeDestroyed;
+        result.templateEngine = options.templateEngine;
+    } else {
+        result.foreach = valueAccessor();
+    }
+
+    //use an afterRender function to add meta-data
+    if (options.afterRender) {
+        //wrap the existing function, if it was passed
+        result.afterRender = function(element, data) {
+            ko.bindingHandlers.sortable.afterRender.call(data, element, data);
+            options.afterRender.call(data, element, data);
+        };
+    } else {
+        result.afterRender = ko.bindingHandlers.sortable.afterRender;
+    }
+
+    //return options to pass to the template binding
+    return result;
+};
+
+//connect items with observableArrays
+ko.bindingHandlers.sortable = {
+    init: function(element, valueAccessor, allBindingsAccessor, data, context) {
+        var value = ko.utils.unwrapObservable(valueAccessor()),
+            templateOptions = prepareTemplateOptions(valueAccessor),
+            sortable = ko.bindingHandlers.sortable,
+            connectClass = value.connectClass || sortable.connectClass,
+            allowDrop = value.allowDrop === undefined ? sortable.allowDrop : value.allowDrop,
+            beforeMove = value.beforeMove || sortable.beforeMove,
+            afterMove = value.afterMove || sortable.afterMove,
+            options = value.options || sortable.options;
+
+        //if allowDrop is an observable or a function, then execute it in a computed observable
+        if (ko.isObservable(allowDrop) || typeof allowDrop == "function") {
+            ko.computed({
+               read: function() {
+                   var value = ko.utils.unwrapObservable(allowDrop),
+                       shouldAdd = typeof value == "function" ? value.call(this, templateOptions.foreach) : value;
+                   ko.utils.toggleDomNodeCssClass(element, connectClass, shouldAdd);
+               },
+               disposeWhenNodeIsRemoved: element
+            }, this);
+        } else {
+            ko.utils.toggleDomNodeCssClass(element, connectClass, allowDrop);
+        }
+
+        //attach meta-data
+        $(element).data("ko_sortList", templateOptions.foreach);
+        $(element).sortable(ko.utils.extend(options, {
+            update: function(event, ui) {
+                var sourceParent, targetParent, targetIndex, arg,
+                    item = ui.item.data("ko_sortItem");
+
+                if (item) {
+                    //identify parents
+                    sourceParent = ui.item.data("ko_parentList");
+                    targetParent = ui.item.parent().data("ko_sortList");
+                    targetIndex = ko.utils.arrayIndexOf(ui.item.parent().children(), ui.item[0]);
+
+                    if (beforeMove || afterMove) {
+                        arg = {
+                            item: item,
+                            sourceParent: sourceParent,
+                            sourceIndex: sourceParent.indexOf(item),
+                            targetParent: targetParent,
+                            targetIndex: targetIndex,
+                            cancelDrop: false
+                        };
+                    }
+
+                    if (beforeMove) {
+                        beforeMove.call(this, arg, event, ui);
+                        if (arg.cancelDrop) {
+                            $(ui.sender).sortable('cancel');
+                            return;
+                        }
+                    }
+
+                    if (targetIndex >= 0) {
+                        sourceParent.remove(item);
+                        targetParent.splice(targetIndex, 0, item);
+                    }
+                    //rendering is handled by manipulating the observableArray; ignore dropped element
+                    ui.item.remove();
+
+                    //allow binding to accept a function to execute after moving the item
+                    if (afterMove) {
+                       afterMove.call(this, arg, event, ui);
+                    }
+                }
+            },
+            connectWith: "." + connectClass
+        }));
+
+        //handle disposal
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+            $(element).sortable("destroy");
+        });
+
+        //we are wrapping the template binding
+        return ko.bindingHandlers.template.init(element, function() { return templateOptions; }, allBindingsAccessor, data, context);
+    },
+    update: function(element, valueAccessor, allBindingsAccessor, data, context) {
+        var templateOptions = prepareTemplateOptions(valueAccessor);
+
+        //call the actual template binding
+        ko.bindingHandlers.template.update(element, function() { return templateOptions; }, allBindingsAccessor, data, context);
+    },
+    afterRender: function(elements, data) {
+        ko.utils.arrayForEach(elements, function(element) {
+            if (element.nodeType === 1) {
+                $(element).data("ko_sortItem", data);
+                $(element).data("ko_parentList", $(element).parent().data("ko_sortList"));
+            }
+        });
+    },
+    connectClass: 'ko_container',
+    allowDrop: true,
+    afterMove: null,
+    beforeMove: null,
+    options: {}
+};
+})(ko, jQuery);
