544f315172cf1561c456d02b73d78c84e6b8c262
Danil Flores <dflor003@gmail.com>
2012-10-08 11:03:52 -0400

    Added support for isAccepted option on sortable.
    Added support for isAccepted option on sortable and changed the overall
    way things were being done to be more model-based than UI-based. Clone
    now takes the model as it's parameter.

--- src/knockout-sortable.js
+++ src/knockout-sortable.js
@@ -2,7 +2,8 @@
     var ITEMKEY = "ko_sortItem",
         LISTKEY = "ko_sortList",
         PARENTKEY = "ko_parentList",
-        DRAGKEY = "ko_dragItem";
+        DRAGKEY = "ko_dragItem",
+        PARENTVMKEY = 'ko_parentvm';
 
     //internal afterRender that adds meta-data to children
     var addMetaDataAfterRender = function(elements, data) {
@@ -97,7 +98,7 @@
 
             //initialize sortable binding after template binding has rendered in update function
             setTimeout(function() {
-                var dragItem;
+                var dragItem, currentParentViewModel;
                 $element.sortable(ko.utils.extend(sortable.options, {
                     start: function(event, ui) {
                         //make sure that fields have a chance to update model
@@ -108,22 +109,53 @@
                     },
                     receive: function(event, ui) {
                         dragItem = ko.utils.domData.get(ui.item[0], DRAGKEY);
-                        if (dragItem && dragItem.clone) {
-                            dragItem = dragItem.clone();
+                    },
+                    sort: function (event, ui) {
+                        // If no isAccepted callback, return
+                        if (!sortable.isAccepted) return;
+                        // Get placeholder and parent DOM elements and make sure they're set
+                        var helper = ui.helper,
+                            placeholderParent = ui.placeholder && ui.placeholder.size() ? ui.placeholder.parent() : null;
+                        if (!placeholderParent.size() || !helper.size()) return;
+                        // Get parent viewmodel and item viewmodel attached and make sure they're set
+                        var parentViewModel = ko.utils.domData.get(placeholderParent[0], PARENTVMKEY),
+                            itemViewModel = ko.utils.domData.get(helper[0], ITEMKEY) || ko.utils.domData.get(helper[0], DRAGKEY);
+                        if (!parentViewModel || !itemViewModel) return;
+                        // If changing the parent that we're dragging over, call isValid to see if it's a valid target
+                        if (!currentParentViewModel || currentParentViewModel !== parentViewModel) {
+                            // Set current parent so we only call isValid once per parent change
+                            currentParentViewModel = parentViewModel;
+                            var isValid = sortable.isAccepted.call(currentParentViewModel, currentParentViewModel, itemViewModel);
+                            // Set placeholder valid/invalid state
+                            var body = $('body');
+                            if (!isValid) {
+                                ui.placeholder.addClass(sortable.invalidDropClass);
+                                body.css('cursor', sortable.invalidDropCursor);
+                            }
+                            else {
+                                ui.placeholder.removeClass(sortable.invalidDropClass);
+                                body.css('cursor', sortable.options.cursor || 'auto');
+                            }
                         }
                     },
                     update: function(event, ui) {
-                        var sourceParent, targetParent, targetIndex, i, targetUnwrapped, arg,
+                        var sourceParent, targetParent, targetParentVm, targetIndex, i, targetUnwrapped, arg,
                             el = ui.item[0],
+                            isDrag = !!dragItem,
                             item = ko.utils.domData.get(el, ITEMKEY) || dragItem;
 
                         dragItem = null;
+                        currentParentViewModel = null;
 
                         if (item) {
                             //identify parents
                             sourceParent = ko.utils.domData.get(el, PARENTKEY);
                             targetParent = ko.utils.domData.get(el.parentNode, LISTKEY);
+                            targetParentVm = ko.utils.domData.get(el.parentNode, PARENTVMKEY);
                             targetIndex = ko.utils.arrayIndexOf(ui.item.parent().children(), el);
+                            if (isDrag && item.clone) {
+                                item = item.clone.call(item, targetParentVm);
+                            }
 
                             //take destroyed items into consideration
                             if (!templateOptions.includeDestroyed) {
@@ -136,7 +168,7 @@
                                 }
                             }
 
-                            if (sortable.beforeMove || sortable.afterMove) {
+                            if (sortable.beforeMove || sortable.afterMove || sortable.isAccepted) {
                                 arg = {
                                     item: item,
                                     sourceParent: sourceParent,
@@ -151,6 +183,20 @@
                             if (sortable.beforeMove) {
                                 sortable.beforeMove.call(this, arg, event, ui);
                                 if (arg.cancelDrop) {
+                                    //call cancel on the correct list
+                                    if (arg.sourceParent) {
+                                        $(arg.sourceParent === arg.targetParent ? this : ui.sender).sortable('cancel');
+                                    }
+                                    //for a draggable item just remove the element
+                                    else {
+                                        $(el).remove();
+                                    }
+                                    return;
+                                }
+                            }
+                            if (sortable.isAccepted) {
+                                var isValid = sortable.isAccepted.call(targetParentVm, targetParentVm, item);
+                                if (isValid === false) {
                                     //call cancel on the correct list
                                     if (arg.sourceParent) {
                                         $(arg.sourceParent === arg.targetParent ? this : ui.sender).sortable('cancel');
@@ -185,6 +231,10 @@
                         if (updateActual) {
                             updateActual.apply(this, arguments);
                         }
+                        // Clear DOM data on helper
+                        if (ui.helper && ui.helper.size()) {
+                            ko.utils.domData.clear(ui.helper[0]);
+                        }
                     },
                     connectWith: sortable.connectClass ? "." + sortable.connectClass : false
                 }));
@@ -212,6 +262,7 @@
 
             //attach meta-data
             ko.utils.domData.set(element, LISTKEY, templateOptions.foreach);
+            ko.utils.domData.set(element, PARENTVMKEY, data);
 
             //call template binding's update with correct options
             ko.bindingHandlers.template.update(element, function() { return templateOptions; }, allBindingsAccessor, data, context);
@@ -220,6 +271,8 @@
         allowDrop: true,
         afterMove: null,
         beforeMove: null,
+        invalidDropClass: 'invalid-drop',
+        invalidDropCursor: 'not-allowed',
         options: {}
     };
 
@@ -231,7 +284,8 @@
                 draggableOptions = ko.utils.extend({}, ko.bindingHandlers.draggable.options),
                 templateOptions = prepareTemplateOptions(valueAccessor, "data"),
                 connectClass = value.connectClass || ko.bindingHandlers.draggable.connectClass,
-                isEnabled = value.isEnabled !== undefined ? value.isEnabled : ko.bindingHandlers.draggable.isEnabled;
+                isEnabled = value.isEnabled !== undefined ? value.isEnabled : ko.bindingHandlers.draggable.isEnabled,
+                startActual = draggableOptions.start;
 
             value = value.data || value;
 
@@ -244,6 +298,14 @@
             //setup connection to a sortable
 
             draggableOptions.connectToSortable = connectClass ? "." + connectClass : false;
+            draggableOptions.start = function (event, ui) {
+                var helper = ui.helper;
+                if (!helper || !helper.size()) return;
+                ko.utils.domData.set(helper[0], DRAGKEY, value);
+                if (startActual) {
+                    startActual.apply(this, arguments);
+                }
+            };
 
             //initialize draggable
             $(element).draggable(draggableOptions);
